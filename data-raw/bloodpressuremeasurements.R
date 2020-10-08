3cdeaf81721b2ffbd8dbb01ae0062da216d969e8
Luis de Sousa <luisd@syeop.co.za>
2020-10-08 13:52:53 +0200

    Update bloodpressuremeasurements.R
    Document dataset

--- data-raw/bloodpressuremeasurements.R
+++ data-raw/bloodpressuremeasurements.R
@@ -1,5 +1,10 @@
 ## code to prepare `bloodpressuremeasurements` dataset goes here
-
+#' Blood pressure measurements.
+#'
+#' An example of blood pressure measurements
+#'
+#' @format A data frame with 42 rows and 6 variables:
+#' 
 bloodpressuremeasurements <- readr::read_csv("data-raw/bloodpressuremeasurements.csv")
 
 usethis::use_data(bloodpressuremeasurements, overwrite = TRUE)
